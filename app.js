3396b4ecc911a5bd10049e08c3efe600e8e862a1
PMarinPy <pablo.marin1@utp.edu.co>
2024-06-23 15:54:50 -0500

    actualización peliculas

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-const cargarPeliculas = () => {
-	const respuesta = fetch('https://api.themoviedb.org/3/movie/157336?api_key=2a94992c3a816def7674d96f6728ff08')
-	console.log(respuesta)
+const cargarPeliculas = async() => {
+	try {
+		const respuesta = await fetch('https://api.themoviedb.org/3//movie/popular?api_key=2a94992c3a816def7674d96f6728ff08');
+		console.log(respuesta);
+    	if(respuesta.status === 200){
+			const datos = await respuesta.json();
+			let peliculas =  ``		
+			datos.results.forEach(pelicula => {
+				peliculas += `
+				<div class="pelicula">
+					<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+					<h3 class="titulo">${pelicula.title}</h3>
+				</div>
+				`
+				console.log(pelicula.title)
+			});
+			document.querySelector("#contenedor").innerHTML = peliculas;
+
+		} else if(respuesta.status === 404) {
+			console.log("La pelicula que buscas no existe.")
+		} else {
+			console.log("Error al cargar la pelicula.")
+		}
+	} catch(error) {
+		console.log('Error al cargar las películas '+error);
+	}
 }
 
 cargarPeliculas()
